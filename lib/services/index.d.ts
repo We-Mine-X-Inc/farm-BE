828a6a6403f1adb78f4f6c0d2ca1f66be74a97ee
adwemine <108937310+adwemine@users.noreply.github.com>
2023-05-24 12:47:47 -0400

    Export necessary services for consuming libraries

---
+++ lib/services/index.d.ts
@@ -0,0 +1,14 @@
+export * from "./coin-market-info.service";
+export * from "./contract.service";
+export * from "./customer.service";
+export * from "./facility-info.service";
+export * from "./inventory-item.service";
+export * from "./mgmt-account.service";
+export * from "./miner-market-info.service";
+export * from "./miner.service";
+export * from "./pool-revenue.service";
+export * from "./pool-worker-hash-rate-contribution.service";
+export * from "./pool.service";
+export * from "./supplier-quote.service";
+export * from "./uptime-tick.service";
+//# sourceMappingURL=index.d.ts.map
